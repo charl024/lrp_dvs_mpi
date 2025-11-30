5cca55fcdd0f30e74b64a68e18a299e1fc0a499c
charl024 <comaoeng2003@gmail.com>
2025-11-29 22:02:06 -0700

    update gitignore and shell script

--- run_py.sh
+++ run_py.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=CS542_test
+#SBATCH --job-name=EB_Test
 #SBATCH --partition=general
 #SBATCH --nodes=2
 #SBATCH --ntasks-per-node=32
-#SBATCH --time=00:10:00
-#SBATCH --output=./out/CS542_test_%j.out
-#SBATCH --error=./out/CS542_test_%j.err
+#SBATCH --time=00:30:00
+#SBATCH --output=./out/EB_test_%j.out
+#SBATCH --error=./out/EB_test_%j.err
 
 module load miniconda3
 module load openmpi
 
 source $(conda info --base)/etc/profile.d/conda.sh
 
-conda activate dv
-
-mpirun -n 64 python ./src/mpi_test.py
+mpirun -n 64 python ./src/main_parallel.py
